cf3c301be399aeca30eb043cc76f8b303f8a82da
AsimoLoveGym <xiazhuangz@gmail.com>
2016-07-27 02:35:19 -0500

    Use data instead of hard coded data in Cat function, it's the 1st step for various cats

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
-var Cat = function(){
-  this.clickCount = ko.observable(0);
-  this.name = ko.observable('Tabby');
-  this.imgSrc = ko.observable('img/miaomiao_1.jpg');
-  this.imgAttribution = ko.observable('https://www.flickr.com/photos/big');
-  this.nicknames = ko.observableArray(['Tabtab','T-Bone','Mr. T','Catty Cat']);
+var Cat = function(data){
+  this.clickCount = ko.observable(data.clickCount);
+  this.name = ko.observable(data.name);
+  this.imgSrc = ko.observable(data.imgSrc);
+  this.imgAttribution = ko.observable(data.imgAttribution);
+  this.nicknames = ko.observableArray(data.nicknames);
 
   this.title = ko.computed(function(){
     var title;
@@ -27,7 +27,13 @@ var Cat = function(){
 }
 
 var ViewModel = function(){
-  this.currentCat = ko.observable(new Cat());
+  this.currentCat = ko.observable(new Cat({
+    clickCount: 0,
+    name: 'Tabby',
+    imgSrc: 'img/miaomiao_1.jpg',
+    imgAttribution: 'https://www.flickr.com/photos/big',
+    nicknames: ['Tabtab','T-Bone','Mr. T','Catty Cat'],
+  }));
   this.incrementCounter = function(){
     this.clickCount(this.clickCount()+1);
   };
